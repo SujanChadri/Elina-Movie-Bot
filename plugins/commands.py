0fda8ea9678f35132f67a651a542cfee6cdcd44c
SujanCh740 <132272276+SujanCh740@users.noreply.github.com>
2025-12-18 13:11:17 +0545

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,8 +49,6 @@ async def start(client, message):
                 ],[
                     InlineKeyboardButton('💠 Hᴇʟᴘ 💠', callback_data='help'),
                     InlineKeyboardButton('♻️ Aʙᴏᴜᴛ ♻️', callback_data='about')
-                ],[
-                    InlineKeyboardButton("🪙 Fʀᴇᴇ Eᴀʀɴɪɴɢ & Cʟᴀɪᴍ Pʀᴇᴍɪᴜᴍ 💰", url=f"https://t.co/TM55SfrEvm")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -109,8 +107,6 @@ async def start(client, message):
                 ],[
                     InlineKeyboardButton('💠 Hᴇʟᴘ 💠', callback_data='help'),
                     InlineKeyboardButton('♻️ Aʙᴏᴜᴛ ♻️', callback_data='about')
-                ],[
-                    InlineKeyboardButton("🪙 Fʀᴇᴇ Eᴀʀɴɪɴɢ & Cʟᴀɪᴍ Pʀᴇᴍɪᴜᴍ 💰", url=f"https://t.co/TM55SfrEvm")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
